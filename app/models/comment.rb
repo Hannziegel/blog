84aa0323dbe7f26f30747c0073b5110faa285601
Rodrigo Johann Reckziegel Nunes <hannziegel@gmail.com>
2022-10-29 13:19:06 -0300

    Create comment model

---
+++ app/models/comment.rb
@@ -0,0 +1,11 @@
+class Comment < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  belongs_to :post
+  after_create :increase_counter
+
+  private
+
+  def increase_counter
+    post.increment!(:comments_counter)
+  end
+end
