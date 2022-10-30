8bba4ae6081e03353470528347f40c13ab55d1fc
Rodrigo Johann Reckziegel Nunes <hannziegel@gmail.com>
2022-10-29 23:05:31 -0300

    Update migrate and schema

--- db/schema.rb
+++ db/schema.rb
@@ -33,8 +33,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_29_054106) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "text"
-    t.integer "comments_counter"
-    t.integer "likes_counter"
+    t.integer "comments_counter", default: 0
+    t.integer "likes_counter", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "author_id", null: false
@@ -45,7 +45,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_29_054106) do
     t.string "name"
     t.text "photo"
     t.text "bio"
-    t.integer "posts_counter"
+    t.integer "posts_counter", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
