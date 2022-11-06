90aa6bc5ec6008723817a38ea32ff3784e1d6cd3
Rodrigo Johann Reckziegel Nunes <hannziegel@gmail.com>
2022-11-06 00:16:31 -0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   get 'users/show'
 
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+    resources :posts, only: [:index, :new, :create, :show] do
+      resources :comments, only: [:new, :create]
+      resources :likes, only: [ :create]
+    end
   end
 end
